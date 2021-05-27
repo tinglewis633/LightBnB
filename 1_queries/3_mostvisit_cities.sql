d4892c05b184a75b046990612d10056e14d97f55
Lewis <tinglewis633@gmail.com>
2021-05-27 03:10:04 +0000

    Connecting to PostgreSQL

--- 1_queries/3_mostvisit_cities.sql
+++ 1_queries/3_mostvisit_cities.sql
@@ -2,5 +2,5 @@ SELECT properties.city, count(reservations) as total_reservations
 FROM reservations
 JOIN properties ON property_id = properties.id
 GROUP BY properties.city
-ORDER BY total_reservations DESC;
-LIMIT 5
+ORDER BY total_reservations DESC
+LIMIT 5;
